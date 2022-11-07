881c63829e5c903ed9554734d18f76fae093e405
Meezaan Davids <meezaandavids365@gamil.com>
2022-11-07 16:24:10 +0200

    first commit

---
+++ js/code.js
@@ -0,0 +1,22 @@
+// function madeBy () {
+//     alert("Hello my name is Meezaan Davids!");
+// }
+// let userName = 'Meezaan';
+// let userAge = '19';
+// let isMale = 'true'
+
+// document.write(userName,"<br>");
+// document.write(userAge,"<br>");
+// document.write(isMale,"<br>");
+
+// let userName = 'Meezaan'
+// let userAge = 19
+// let isMale = true
+
+// document.write(userName);
+// document.write("<br>" +userAge);
+// document.write("<br>" +isMale);
+// document.write(`<br>Name: ${userName}<br>Age: ${userAge}<br>Male: ${isMale}`)
+
+// let names = ['Messi', 'Suarez', 'Neymar']
+
